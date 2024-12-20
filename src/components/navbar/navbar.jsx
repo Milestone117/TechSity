1fc6bb7a0d79e6d06044aa79a2671e682d2c683f
milestone117 <iddy2525@gmail.com>
2024-12-20 07:07:07 +0300

    funyyyy

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,77 +1,37 @@
-import './navbar.css'
-
-import logo from '../../assets/logo.png'
-
-import menu_icon from '../../assets/menu-icon.png'
-
-import { useState } from 'react'
-
-import { useEffect } from 'react'
-
+import styles from './navbar.module.css'
+import logo from '@/assets/logo.png'
+import menu_icon from '@/assets/menu-icon.png'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-scroll'
 
-
-
 const Navbar = () => {
-
-
-
     const [sticky, setSticky] = useState(false)
+    const [mobileMenu, setMobileMenu] = useState(false)
 
-
-
-    useEffect(()=>{
-
-        window.addEventListener('scroll',()=>{
-
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
             window.scrollY > 600 ? setSticky(true) : setSticky(false)
-
         })
-
-    },[])
-
-
-
-    const [mobileMenu, setMobileMenu] = useState(false);
+    }, [])
 
     const toggleMenu = () => {
-
-        mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
-
+        setMobileMenu(!mobileMenu)
     }
 
-
-
-  return (
-
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-
-        <img src={logo} alt=""  className='logo'/>
-
-        <ul className={mobileMenu?'': 'hide-mobile-menu'}>
-
-            <li><Link to='hero' smooth={true} offset={0} duration={500}>home</Link></li>
-
-            <li><Link to='program' smooth={true} offset={-260} duration={500}>program</Link></li>
-
-            <li><Link to='about' smooth={true} offset={-150} duration={500}>about us</Link></li>
-
-            <li><Link to='campus' smooth={true} offset={-260} duration={500}>campus</Link></li>
-
-            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>testimonials</Link></li>
-
-            <li><Link to='contact' smooth={true} offset={-260} duration={500}>contact us</Link></li>
-
-        </ul>
-
-        <img src={menu_icon} alt=""  className='menu-icon' onClick={toggleMenu}/>
-
-    </nav>
-
-  )
-
+    return (
+        <nav className={`${styles.container} ${sticky ? styles.darkNav : ''}`}>
+            <img src={logo} alt="logo" className={styles.logo}/>
+            <ul className={`${styles.menu} ${!mobileMenu ? styles.hideMobileMenu : ''}`}>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>about us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500}>contact us</Link></li>
+            </ul>
+            <img src={menu_icon} alt="menu" className={styles.menuIcon} onClick={toggleMenu}/>
+        </nav>
+    )
 }
-
-
 
 export default Navbar
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ytechsity/',
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      scopeBehaviour: 'local',
+    },
+  },
   build: {
+    sourcemap: true,
+    cssCodeSplit: true,
     rollupOptions: {
-      external: ['react-scroll'],
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
